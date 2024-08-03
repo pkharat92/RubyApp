ff932ec437be2c73d121383c7a6417d6e7f8e94b
pkharat92 <pkharat92@gmail.com>
2024-08-03 15:25:25 -0700

    Updated unit tests for controllers

--- test/controllers/draft_picks_controller_test.rb
+++ test/controllers/draft_picks_controller_test.rb
@@ -1,12 +1,13 @@
 require "test_helper"
 
 class DraftPicksControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
+  test "get show success" do
     get draftpicks_url, params:
     {
       "selected-team-1" => "Los Angeles Chargers",
       "selected-team-2" => "Baltimore Ravens"
     }
+
     assert_response :success
   end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test "get index success" do
     get home_url
     assert_response :success
   end
